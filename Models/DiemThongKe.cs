a65334d3b7875db8fabbef33dbf78836a4ef496f
khoavo090807 <khoavo090807@gmail.com>
2025-10-26 19:41:28 +0700

    Add files via upload

---
+++ Models/DiemThongKe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuanLySinhVien.Models
+{
+    public class DiemThongKe
+    {
+        public string MaSV { get; set; }
+        public string HoTenSV { get; set; }
+        public string MaLHP { get; set; }
+        public string TenMH { get; set; }
+        public int SoTinChi { get; set; }
+
+        public float? DiemChuyenCan { get; set; }
+        public float? DiemGiuaKy { get; set; }
+        public float? DiemCuoiKy { get; set; }
+        public float? DiemTongKet { get; set; }
+
+        public string XepLoai { get; set; }
+        public DateTime NgayDangKy { get; set; }
+    }
+}
--- Models/Khoa.cs
+++ Models/Khoa.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace QuanLySinhVien.Models
 {
     public class Khoa
     {
         [Display(Name = "Mã khoa")]
+        [Required(ErrorMessage = "Mã khoa không được để trống")]
         public string MaKhoa { get; set; }
 
         [Display(Name = "Tên khoa")]
+        [Required(ErrorMessage = "Tên khoa không được để trống")]
+        [StringLength(100)]
         public string TenKhoa { get; set; }
 
         [Display(Name = "Số điện thoại")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
         public string SoDienThoai { get; set; }
+
+        
+
+        // Navigation properties
+        public virtual ICollection<Lop> DanhSachLop { get; set; }
+        public virtual ICollection<GiangVien> DanhSachGiangVien { get; set; }
     }
 }

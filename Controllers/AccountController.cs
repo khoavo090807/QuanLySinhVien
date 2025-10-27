9553750db6565424c7a09005cb19e87d3734e554
khoavo090807 <khoavo090807@gmail.com>
2025-10-27 20:49:38 +0700

    Add files via upload

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -270,7 +270,12 @@ namespace QuanLySinhVien.Controllers
             using (SHA256 sha256 = SHA256.Create())
             {
                 byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                return Convert.ToBase64String(hashedBytes);
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in hashedBytes)
+                {
+                    builder.Append(b.ToString("x2")); // Chuyển thành hexadecimal
+                }
+                return builder.ToString();
             }
         }
 
